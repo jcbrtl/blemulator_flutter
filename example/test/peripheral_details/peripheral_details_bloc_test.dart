29c796285a9148342cca94e5e1cb99cf58bfcbff
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-06 12:13:29 +0100

    WIP

--- example/test/peripheral_details/peripheral_details_bloc_test.dart
+++ example/test/peripheral_details/peripheral_details_bloc_test.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:blemulator_example/model/ble_peripheral.dart';
 import 'package:blemulator_example/peripheral_details/bloc.dart';
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../mock/mocks.dart';
@@ -9,21 +12,56 @@ void main() {
   PeripheralDetailsBloc peripheralDetailsBloc;
   MockBleAdapter bleAdapter;
   BlePeripheral peripheral;
+  StreamController<PeripheralConnectionState>
+      peripheralConnectionStateStreamController;
 
   setUp(() {
     bleAdapter = MockBleAdapter();
     peripheral = SampleBlePeripheral();
+    peripheralDetailsBloc = PeripheralDetailsBloc(bleAdapter, peripheral);
+    peripheralConnectionStateStreamController = StreamController();
 
-    peripheralDetailsBloc =
-        PeripheralDetailsBloc(bleAdapter, peripheral);
   });
+
+  void receiveEvent(PeripheralDetailsEvent event) {
+    peripheralDetailsBloc.add(event);
+  }
+
+  void firePeripheralConnectionStateFromAdapter(
+      PeripheralConnectionState connectionState) {
+    peripheralConnectionStateStreamController.sink.add(connectionState);
+  }
 
   tearDown(() {
     peripheralDetailsBloc.close();
     bleAdapter = null;
+    peripheralConnectionStateStreamController.close();
   });
 
   test('initial state contains peripheral provided in the constructor', () {
     expect(peripheralDetailsBloc.initialState.peripheral, peripheral);
   });
+
+  group('receiving ConnectToPeripheral event', () {
+    test('emits PeripheralDetailsState if no BleError was thrown', () {});
+
+    test('emits PeripheralDetailsErrorState if BleError was thrown', () {});
+  });
+
+  group('receiving DisconnectFromPeripheral event', () {
+    test('emits PeripheralDetailsState if no BleError was thrown', () {});
+
+    test('emits PeripheralDetailsErrorState if BleError was thrown', () {});
+  });
+
+  group('receiving CheckConnectionState', () {
+    test('emits PeripheralDetailsState if no BleError was thrown', () {});
+
+    test('emits PeripheralDetailsErrorState if BleError was thrown', () {});
+  });
+
+  test(
+      'receiving ConnectionStateUpdate event emits PeripheralDetailsState with '
+      'updated PeripheralConnectionState',
+      () {});
 }
